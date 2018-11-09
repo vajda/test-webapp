5e59651a5471a2b392736f50608ddbf3e6e4bfda
Vladimir Vajda <vladimir.vajda@smartcat.io>
2018-11-09 17:03:55 +0100

    minor change just to check

--- backend/routers/default.js
+++ backend/routers/default.js
@@ -6,8 +6,8 @@ const router = express();
 router.get('/test', (req, res) => {
   const vals = config.get("root.vals");
   res.status(200).json({
-    a: vals.prefixA + " A " +vals.sufixA,
-    b: vals.prefixB + " B " + vals.sufixB
+    a: vals.prefixA + " AAAA " +vals.sufixA,
+    b: vals.prefixB + " BBBB " + vals.sufixB
   });
 });
 
